e2f1c576b3c86cf3c1855456bd1097169a9234fe
jhunkeler <jhunkeler@c5b2fb0a-d05d-0410-98c8-828840a80ff6>
2010-01-27 22:55:50 +0000

    Cleaning up and reordering directory structure.
    git-svn-id: http://svn.3rd-infantry-division.org/testing/branches/Bayonet CMS v2.1@475 c5b2fb0a-d05d-0410-98c8-828840a80ff6

--- functions.js
+++
@@ -1,8 +0,0 @@
-function gotoEditPage(id)
-{
-       var pageid = document.getElementById(id).value;
-       if(pageid == 0)
-      	 location.href="?load=admin&op=pages";
-	   else
-  		 location.href="?load=admin&op=pages&edit="+pageid;
-}
