fbbaf67f321bd7da4892257807683de90f24dd19
thirdid <thirdid@c5b2fb0a-d05d-0410-98c8-828840a80ff6>
2010-01-31 23:35:41 +0000

    Screw up again again....
    git-svn-id: http://svn.3rd-infantry-division.org/testing/branches/Bayonet CMS v2.1@486 c5b2fb0a-d05d-0410-98c8-828840a80ff6

--- blocks/index.php
+++
@@ -1,6 +0,0 @@
-<ul class="block">
-<li><?php echo LinkModule('rudi',NULL,'Roster'); ?></li>
-<li><?php echo LinkModule('rudi','&amp;show=awards','Awards &amp; Medals'); ?></li>
-<li><?php echo LinkModule('rudi','&amp;show=ranks','Ranks'); ?></li>
-</ul>
-
