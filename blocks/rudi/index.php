c0a7ae21a71da69b2d412a0e470310c9d85df72d
jhunkeler <jhunkeler@c5b2fb0a-d05d-0410-98c8-828840a80ff6>
2010-01-31 23:35:02 +0000

    Screwed up again.
    git-svn-id: http://svn.3rd-infantry-division.org/testing/branches/Bayonet CMS v2.1@485 c5b2fb0a-d05d-0410-98c8-828840a80ff6

---
+++ blocks/rudi/index.php
@@ -0,0 +1,6 @@
+<ul class="block">
+<li><?php echo LinkModule('rudi',NULL,'Roster'); ?></li>
+<li><?php echo LinkModule('rudi','&amp;show=awards','Awards &amp; Medals'); ?></li>
+<li><?php echo LinkModule('rudi','&amp;show=ranks','Ranks'); ?></li>
+</ul>
+
